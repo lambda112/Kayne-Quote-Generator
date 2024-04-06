b0d13135e33d85b9fae354307c8afea03b852b1a
Kamran Azim <Kamazim121212@gmail.com>
2024-04-06 02:01:46 +0100

    Created working kanye quote generator

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ def generate_text():
     content = response.json()["quote"]
     canvas.itemconfigure(text, text=content)
 
-# Labels
-
 
 # Canvas 
 canvas = Canvas(height=500, width=500)
@@ -23,6 +21,7 @@ canvas.config(background="#141312")
 # Background
 image = ImageTk.PhotoImage(Image.open("image.jpeg"), size=(550, 500))
 canvas.create_image(250, 250, image = image)
+canvas.create_text(250, 80, text = "Kanye Quote Generator", fill="white", font=("arial", 25, "bold"))
 text = canvas.create_text(250, 250,text="Generate Quote With The Kanye Button!", font=("arial", 16, "bold"), fill="white", width=260)
 
 # Button
