510c18b8f33532f330d77189743cb65750a3745f
Kamran Azim <Kamazim121212@gmail.com>
2024-04-06 01:55:47 +0100

    Created button to generate quote

--- main.py
+++ main.py
@@ -6,16 +6,28 @@ window = Tk()
 window.config(background="black")
 window.title("Kanye Quote Generator")
 
-response = requests.get(url = "https://api.kanye.rest")
-response.raise_for_status()
-content = response.json()["quote"]
-print(content)
+def generate_text():
+    response = requests.get(url = "https://api.kanye.rest")
+    response.raise_for_status()
+    content = response.json()["quote"]
+    canvas.itemconfigure(text, text=content)
 
+# Labels
+
+
+# Canvas 
 canvas = Canvas(height=500, width=500)
-canvas.grid(row = 0,column=0)
+canvas.grid(row = 1, column=0)
 canvas.config(background="#141312")
+
+# Background
 image = ImageTk.PhotoImage(Image.open("image.jpeg"), size=(550, 500))
 canvas.create_image(250, 250, image = image)
-canvas.create_text(250,250,text=content, font=("arial", 20, "bold"), fill="white", width=260)
+text = canvas.create_text(250, 250,text="Generate Quote With The Kanye Button!", font=("arial", 16, "bold"), fill="white", width=260)
+
+# Button
+kanye_image = ImageTk.PhotoImage(Image.open("kanye.jpg"), size=(100, 100))
+kanye_button = Button(image=kanye_image, command=generate_text)
+canvas.create_window(250, 500, window = kanye_button, anchor="s")
 
 window.mainloop()
